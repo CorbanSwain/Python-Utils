abbf8fe83dd978a444ff7252bf67351b8a254b35
Corban Swain <CorbanSwain@gmail.com>
2019-02-18 00:54:21 -0500

    adding recur to files.touchdir

--- files.py
+++ files.py
@@ -6,9 +6,17 @@ import os
 __all__ = ['touchdir', 'make_str_filesafe']
 
 
-def touchdir(pth):
+def touchdir(pth, recur=True):
     try:
         os.mkdir(pth)
+    except FileNotFoundError:
+        if recur:
+            head, tail = os.path.split(pth)
+            if head:
+                touchdir(head)
+                touchdir(pth)
+        else:
+            raise
     except FileExistsError:
         pass
 
